db3a063b1723b3082a02b2a860d22544714f6d5b
Lior Nevo <nevo.lior@gmail.com>
2021-01-19 16:25:16 -0500

    WIP

--- docker/init.sh
+++ docker/init.sh
@@ -7,7 +7,7 @@ COLOR_GREEN=32
 COLOR_RED=31
 COLOR_YELLOW=33
 COLOR_BLUE=34
-FOUND_FILES=false
+SCANNED=false
 
 printColor() {
     local COLOR=29
@@ -28,11 +28,19 @@ trapHandler() {
 trap '[ $? -eq 0 ] && exit 0 || trapHandler ${LINENO} $?' EXIT
 
 execute_scan() {
-    printColor "Scanning $1, saving reports to $2.html" $COLOR_GREEN
-    pa11y --reporter html "$1" > /opt/output/$2.html || true
-    printColor "Scanning $1, saving reports to $2.csv" $COLOR_GREEN
-    pa11y --reporter csv "$1" > /opt/output/$2.csv || true
+    SCANNED="true"
+    timestamp=$(date -u +%m_%d_%Y_%H_%M)
+    printColor "Scanning $1, saving reports to $2_${timestamp}.html" $COLOR_GREEN
+    pa11y --reporter html --screen-capture /opt/output/$2_${timestamp}.png "$1" > /opt/output/$2_${timestamp}.html || true
+    printColor "Scanning $1, saving reports to $2_${timestamp}.csv" $COLOR_GREEN
+    pa11y --reporter csv "$1" > /opt/output/$2_${timestamp}.csv || true
     printColor "Done" $COLOR_PINK
+}
+
+get_name_from_url() {
+    name=$(echo $1 | sed -e "s/https:\/\///g" | sed -e "s/http:\/\///g")
+    name=$(echo $name | sed -e "s/\./_/g" | sed -e "s/\//_/g")
+    echo $name
 }
 
 scan_from_urls_file() {
@@ -42,10 +50,9 @@ scan_from_urls_file() {
     else
         while IFS="" read -r p || [ -n "$p" ]; do 
             if [ ! -z "$p" ]; then
-                timestamp=$(date -u +%m_%d_%Y)
                 url=$(echo $p | awk '{print $1}')
-                name=$(echo $p | awk '{print $2}')
-                execute_scan $url ${name}_${timestamp}
+                name=$(get_name_from_url $url)
+                execute_scan $url ${name}
             fi
         done < "$URL_FILE"
     fi
@@ -55,9 +62,8 @@ scan_from_input_path() {
     printColor "Scanning from input path ${1}" $COLOR_YELLOW
     for file in ${1}/*.{htm,html}; do
         if [ -f "${file}" ]; then
-            timestamp=$(date -u +%m_%d_%Y)
             name=$(echo $file | sed -e "s/\//_/g" | sed -e "s/\./_/g")
-            execute_scan $file ${name}_${timestamp}
+            execute_scan $file ${name}
         fi
     done
     for directory in ${1}/*; do
@@ -69,12 +75,23 @@ scan_from_input_path() {
 
 scan() {
     pushd /opt/scanner
+    if [ ! -z "${URL}" ]; then
+        name=$(get_name_from_url ${URL})
+        execute_scan ${URL} ${name}
+    fi
     if [ ! -z "${URL_FILE}" ]; then
         scan_from_urls_file
     fi
     scan_from_input_path /opt/input
-    if [ "${FOUND_FILES}" != "true" ]; then
-        printColor "No files were scanned. See documentation" $COLOR_RED
+    if [ "${SCANNED}" != "true" ]; then
+        printColor "Nothing was scanned. See documentation" $COLOR_RED
+        printColor "You should at least run with a URL environment variable:" $COLOR_RED
+        printColor "docker run \\"
+        printColor "    --rm \\"
+        printColor "    -v ${PWD}/output:/opt/output \\"
+        printColor "    -e URL=https://www.mishpatech.co.il \\"
+        printColor "    --name scanner \\"
+        printColor "    scanner"
     fi
 }
 
--- docker/run_scan.sh
+++ docker/run_scan.sh
@@ -5,5 +5,7 @@ docker run \
     -v ${PWD}/output:/opt/output \
     -v ${PWD}/input:/opt/input \
     -e URL_FILE=/opt/input/urls.txt \
+    -e INPUT_PATH=/opt/input \
+    -e URL=https://www.mishpatech.co.il \
     --name scanner \
     scanner
